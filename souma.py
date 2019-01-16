2917f181c26a0e71dc6ac62f7646ffd061b7276e
ach666 <46721480+ach666@users.noreply.github.com>
2019-01-16 11:35:35 -0500

    Update souma.py

--- souma.py
+++ souma.py
@@ -6,11 +6,9 @@ client = discord.Client()
 
 async def my_background_task():
     await client.wait_until_ready()
-    counter = 0
-    channel = discord.Object(id='channel_id_here')
+    channel = discord.Object(id='531423912538537995')
     while not client.is_closed:
-        counter += 1
-        await client.send_message(channel, counter)
+        await client.send_message(channel, "sword")
         await asyncio.sleep(10) # task runs every 60 seconds
 
 @client.event
