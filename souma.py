fb2f79a9d7452b10375f838beca9d39ba08342d0
ach666 <46721480+ach666@users.noreply.github.com>
2019-03-19 18:40:02 -0400

    Update souma.py

--- souma.py
+++ souma.py
@@ -1,14 +1,18 @@
 import discord
 import os
 import asyncio
+import random
 
 client = discord.Client()
+
+sayings = ["do not lament over the rotten souls of those you hath slain in thy battlefield", "you mean a lot  to me", "i will be  your samurai in times of darkness.", "adonith-dono hhot."]
 
 async def my_background_task():
     await client.wait_until_ready()
     channel = discord.Object(id='542037405356523528')
     while not client.is_closed:
         await client.send_message(channel, "buy your meds")
+        await client.send_message(channel, random.choice(sayings))
         await asyncio.sleep(5000)
 
 @client.event
