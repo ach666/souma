de0f278b9573452506bddee7d6547c7518ee3334
ach666 <46721480+ach666@users.noreply.github.com>
2019-03-24 18:30:32 -0400

    Update souma.py

--- souma.py
+++ souma.py
@@ -5,13 +5,13 @@ import random
 
 client = discord.Client()
 
-sayings = ["do not lament over the rotten souls of those you hath slain in thy battlefield", "you mean a lot  to me", "i will be  your samurai in times of darkness.", "adonith-dono hhot.", "you are always doing your best. I am proud of you", "you will reach your dreams with your own power, but i will be willing to help you along the way if you need.", "i shall give you my blessings for a long, fruitfull and fulfilling life."]
+sayings = ["do not lament over the rotten souls of those you hath slain in thy battlefield", "you mean a lot  to me", "i will be  your samurai in times of darkness.", "adonith-dono hhot.", "you are always doing your best. I am proud of you", "you will reach your dreams with your own power, but i will be willing to help you along the way if you need.", "i shall give you my blessings for a long, fruitfull and fulfilling life.", "you have so much potential. don't give up no matter what","I shall be with you with every step you take because i have put my full belief in you", "you rocku"]
 
 async def my_background_task():
     await client.wait_until_ready()
     channel = discord.Object(id='542037405356523528')
     while not client.is_closed:
-        await client.send_message(channel, "buy your meds")
+        await client.send_message(channel, "take your meds")
         await client.send_message(channel, random.choice(sayings))
         await asyncio.sleep(5000)
 
